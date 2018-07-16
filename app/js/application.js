ad965e9c5883ec4289de156e90351685e90ab99c
merethlot <elen.merethlot@gmail.com>
2018-07-16 09:56:08 +0700

    pages

--- app/js/application.js
+++ app/js/application.js
@@ -3,7 +3,9 @@
 function prepare_modals() {
   $('.js-open-modal').click(function () {
       let target = $(this).data('target');
-      $(`#${target}`).fadeIn(400);
+      console.log(target);
+      $('.modal').fadeOut(100);
+      $(`${target}`).fadeIn(400);
       $('.blur').css('filter', 'blur(3px)');
       document.onmousewheel = document.onwheel = function () {
           return false;
@@ -11,7 +13,7 @@ function prepare_modals() {
   })
   if ($(window).width() <= '750') {
       $('.js-open-modal').click(function () {
-          $(`#${target}`).fadeIn(400);
+          $(`${target}`).fadeIn(400);
           $('.blur').css('filter', 'blur(3px)');
           $('body').css('overflow', 'hidden');
           document.onmousewheel = document.onwheel = function () {
@@ -23,7 +25,8 @@ function prepare_modals() {
       var div = $(".modal__content");
       if (!div.is(e.target)
               && div.has(e.target).length === 0
-              || $('.modal__close').is(e.target)) {
+              || $('.js-close-modal').is(e.target)) {
+          $('.modal').fadeOut(100);
           $('.modal').fadeOut(400)
           $('.blur').css('filter', '')
           $('body').css('overflow', '')
@@ -39,5 +42,22 @@ $(document).ready(function () {
   prepare_modals();
   var Switchery = require('switchery');
   var elem = document.querySelector('.js-switch');
-  var init = new Switchery(elem, { color: '#ff4222', className: 'switchery switchery-sm' });
+  if (elem) {
+    var init = new Switchery(elem, { color: '#ff4222', className: 'switchery switchery-sm' });
+  }
+  $(".js-show-answer").click(function () {
+    $(this).parents(".faq-question").addClass("active");
+    let cl = $(this).parents(".faq-show-answer").siblings(".js-answer");
+    cl.show("slow");
+    // cl.css('height', 'auto');
+    $(this).hide();
+  });
+  $(".js-hide-answer").click(function () {
+    $(this).parents(".faq-question").removeClass("active");
+    let link = $(this).parents(".faq-question").find(".js-show-answer");
+    let cl = $(this).parents(".js-answer");
+    // cl.css('height', '0');
+    cl.hide("slow");
+    link.show();
+  })
 })
